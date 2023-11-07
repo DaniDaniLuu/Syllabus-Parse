b9fbe1aad84e1a15d95107a0d68d92db9dbe39ae
DaniMeloni <122004295+DaniDaniLuu@users.noreply.github.com>
2023-11-07 02:07:07 -0600

    Current

--- core/urls.py
+++ core/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('NOT_LOGGED_IN.urls')),
+    path('pdf/', include('PDF_HANDLING.urls'))
 ]
